ee365d93c05e4dd4950f8be25a3a1ba2610b6603
Lin Ochoa <linochoa11690@gmail.com>
2015-03-19 19:55:14 -0400

    my first commit

---
+++ LinExample.cpp
@@ -0,0 +1,39 @@
+//Lin Example
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int resultMax = 0;
+
+bool palindromeChecker(int result){
+	int counter = 0;
+	int temp = result;
+	int 
+	while(temp > 0){
+
+		temp /= 10;
+		counter++;
+	}
+	if(counter % 2 == 0){
+		
+	}
+
+}
+
+int main(){
+
+	int firstNum = 1000;
+	int secondNum = 1000;
+	int resultMax;
+
+	while(firstNum < 1000){
+		while(secondNum < 1000){
+			result = firstNum * secondNum;
+			if(palindromeChecker){
+
+			}
+		}
+	}
+	return 0;	
+}
