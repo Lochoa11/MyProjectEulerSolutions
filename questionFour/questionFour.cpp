cfed3e51498a60d91f9df4c1fc92a030d9a0993d
Lin Ochoa <linochoa11690@gmail.com>
2015-04-05 20:41:41 -0400

    question four is working fully now

--- questionFour/questionFour.cpp
+++ questionFour/questionFour.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 
-bool isPalindrome(int x){
+bool isPalindrome(int x){	
+	
 	int temp = x;
 	int revNum = 0;
-	while(temp > 0){
-		revNum = temp % 10;
+	
+	while(true){
+		revNum += temp % 10;
+		temp /= 10;
+		if(temp <= 0)break;
 		revNum *= 10;
-		temp %= 10;
 	}
 
+	
 	temp = x;
 
 	while(temp > 0){
 		if(revNum % 10 == temp % 10){
-			revNum %= 10;
-			temp %= 10;
+			revNum /= 10;
+			temp /= 10;
 			continue;
 		}
 		return false;
@@ -26,7 +30,6 @@ bool isPalindrome(int x){
 }
 
 bool isEvenDigits(int x){
-	cout << "In isEvenDigits function" << endl;
 	int numberOfXDigets = 0;
 	int temp = x;
 
@@ -52,24 +55,25 @@ int main(){
 	while(firstNum < 1000){//increments first number
 		int secondNum = 100;//recreates second number
 		secondNum += startingPoint;//skips numbers that have been used before (1*2 vs 2*1)
-		if(secondNum >= 1000){//tests to see if second number is over 1000
-			continue;
-		}
-		x = firstNum * secondNum;//actual multiplication stored in x
-		
-
-		if(isEvenDigits(x) == true) {
-			cout << "in isEvenDigits if" << endl;
-			if(isPalindrome(x) == true){
-				cout << "In isPalindrome is now" << endl;
-					if(x > maxPalindrome){
-						maxPalindrome = x;
-						cout << "Palindrome found" << endl;
-						cout << "First number: " << firstNum << endl;
-						cout << "Second number: " << secondNum << endl;
-						cout << "Their Palindrome is: " << x << endl;
-					}
+		while(secondNum < 1000){
+			if(secondNum >= 1000){//tests to see if second number is over 1000
+				continue;
 			}
+			x = firstNum * secondNum;//actual multiplication stored in x
+			
+
+			if(isEvenDigits(x) == true) {
+				if(isPalindrome(x) == true){
+						if(x > maxPalindrome){
+							maxPalindrome = x;
+							cout << "Palindrome found!!!!!!!!!" << endl;
+							cout << "First number: " << firstNum << endl;
+							cout << "Second number: " << secondNum << endl;
+							cout << "Their Palindrome is: " << x << endl;
+						}
+				}
+			}
+			secondNum++;
 		}
 		firstNum++;
 		startingPoint++;
